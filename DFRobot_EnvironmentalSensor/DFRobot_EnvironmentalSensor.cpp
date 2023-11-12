9422c5635102cea8cb45eec7c00eab8517cfc299
harry <harrybest800@gmail.com>
2023-11-12 22:04:15 +0000

    Finish inital draft

--- DFRobot_EnvironmentalSensor/DFRobot_EnvironmentalSensor.cpp
+++ DFRobot_EnvironmentalSensor/DFRobot_EnvironmentalSensor.cpp
@@ -13,5 +13,24 @@ void DFRobotEnvironmentalSensorComponent::setup() {
   environment_.begin();
   ESP_LOGD(TAG, "Conected to environmental sensor");
 };
+
+void DFRobotEnvironmentalSensorComponent::update() {
+  ESP_LOGV(TAG, "Starting update");
+  float temperature = environment_.getTemperature(TEMP_C);
+  temperature_sensor_->publish_state(temperature);
+
+  int pressure = environment_.getAtmospherePressure(HPA);
+  pressure_sensor_->publish_state(pressure);
+
+  auto humidity = environment_.getHumidity();
+  humidity_sensor_->publish_state(humidity);
+
+  auto uv = environment_.getUltravioletIntensity();
+  uv_sensor_>publish_state(uv);
+
+  auto lumens = environment_.getLuminousIntensity();
+  light_sensor_>publish_state(lumens);
+  ESP_LOGV(TAG, "Published readings");
+}
 }  // namespace dfrobotenvironmentalsensor
 }  // namespace esphome
---
+++ DFRobot_EnvironmentalSensor/sensor.py
@@ -0,0 +1,50 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import sensor
+
+from esphome.const import (
+    CONF_HUMIDITY,
+    CONF_ID,
+    CONF_IIR_FILTER,
+    CONF_OVERSAMPLING,
+    CONF_PRESSURE,
+    CONF_TEMPERATURE,
+    DEVICE_CLASS_HUMIDITY,
+    DEVICE_CLASS_PRESSURE,
+    DEVICE_CLASS_TEMPERATURE,
+    STATE_CLASS_MEASUREMENT,
+    UNIT_CELSIUS,
+    UNIT_HECTOPASCAL,
+    UNIT_PERCENT,
+)
+
+dfrobotenvironmentalsensor_ns = cg.esphome_ns.namespace("dfrobotenvironmentalsensor")
+
+DFRobotEnvironmentalSensorComponent = dfrobotenvironmentalsensor_ns.class_(
+    "DFRobotEnvironmentalSensorComponent", cg.PollingComponent
+)
+
+CONFIG_SCHEMA = (
+    cv.Schema(
+        {
+            cv.GenerateID(): cv.declare_id(DFRobotEnvironmentalSensorComponent)
+        }
+    )
+    .extend(cv.polling_component_schema("60s"))
+)
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
+
+    if temperature_config := config.get(CONF_TEMPERATURE):
+        sens = await sensor.new_sensor(temperature_config)
+        cg.add(var.set_temperature_sensor(sens))
+
+    if pressure_config := config.get(CONF_PRESSURE):
+        sens = await sensor.new_sensor(pressure_config)
+        cg.add(var.set_pressure_sensor(sens))
+
+    if humidity_config := config.get(CONF_HUMIDITY):
+        sens = await sensor.new_sensor(humidity_config)
+        cg.add(var.set_humidity_sensor(sens))
